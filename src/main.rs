6a254a847e015432b474e78b07759c5a5c13135b
NomanNasirMinhas <sp17bsse010@gmail.com>
2023-10-29 10:49:46 +0500

    Not Found Error Fixed

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
 use clap::{arg, command, value_parser, ArgAction, Command, Arg};
+use std::{
+    env,
+    error::Error,
+    fs::File,
+    io::{BufRead, BufReader},
+    };
+use std::sync::{Arc, Mutex, Condvar};
+use std::sync::mpsc;
 use sha2::{Sha256, Sha512, Digest};
 #[derive(Debug)]
 enum HashType {
@@ -52,8 +60,12 @@ fn main() {
     let hash_type = get_hash_type(hash.as_ref());    
 
     // Maximum number of threads
-    if threads.parse::<usize>().unwrap() > 10 {
-        panic!("Maximum number of threads is 10");
+    if threads.parse::<usize>().unwrap() > 100 {
+        panic!("Maximum number of threads is 100");
+    }
+
+    if threads.parse::<usize>().unwrap() < 1 {
+        panic!("Minimum number of threads is 1");
     }
     println!("Detected Hash Type: {:?}", hash_type);
     
@@ -65,31 +77,98 @@ fn main() {
     };
     
     // Read dictionary file
-    let mut dict = std::fs::read_to_string(dict).expect("Error reading dictionary file");    
-    dict = dict.replace("\r", "");
-    let mut dict: Vec<&str> = dict.split("\n").collect();
-
-    // Loop through dictionary and check if hash matches
-    let mut found = false;
-
+    let wordlist_file = File::open(dict).expect("Error opening dictionary file");
+    let reader = BufReader::new(&wordlist_file);
+    let words = reader.lines().map(|l| l.unwrap()).collect::<Vec<String>>();
+    println!("Total words in dictionary: {}", words.len());
+    
     // Start timer
+    let mut handles = vec![];    
+    let mut chunk_size = words.len() / threads.parse::<usize>().unwrap();
+    if chunk_size == 0 {
+        chunk_size = 1;
+    }
+    let (sender, receiver) = mpsc::channel();
+    let stop_signal = Arc::new((Mutex::new(false), Condvar::new()));
+    let not_found_count = Arc::new((Mutex::new(0), Condvar::new()));;
     let now = std::time::Instant::now();
-    for (i, word) in dict.iter().enumerate() {
-        let word = word.trim();
-        let word_hash = input_hash_fn(word);
-        if word_hash == hash {
-            println!("Found match: {}", word);
-            found = true;
-            break;
-        }
-        if index {
-            println!("{}: {}", i, word);
-        }
+    let mut iterations = words.chunks(chunk_size).len();
+    for chunk in words.chunks(chunk_size) {
+        let sender = sender.clone();
+        let stop_signal = Arc::clone(&stop_signal);
+        let not_found_count = Arc::clone(&not_found_count);
+        let chunk = chunk.to_vec();
+        let input_hash_fn = input_hash_fn.clone();
+        let hash = hash.clone();        
+        let index = index.clone();
+        let handle = std::thread::spawn(move || {
+            let iterations = iterations.clone();
+            let mut found = false;
+            let hash_index: Arc<Mutex<Vec<String>>> = Arc::new(Mutex::new(vec![]));
+            for (i, word) in chunk.iter().enumerate() {
+                let word = word.trim();                
+                let word_hash = input_hash_fn(word);
+                if index {
+                    let mut hash_index = hash_index.lock().unwrap();
+                    hash_index.push(format!(
+                        "{}: {}",
+                        word_hash,
+                        word
+                    ));                    
+                }
+                if word_hash == hash {
+                    found = true;
+                    println!("Found match: {}", word);  
+                    let (lock, cvar) = &*stop_signal;
+                    let mut stop = lock.lock().unwrap();
+                    *stop = true;
+                    cvar.notify_all();                                  
+                    break;
+                }                
+            }
+            if !found {
+                // print with thread id
+                let (lock, cvar) = &*not_found_count;
+                let mut count = lock.lock().unwrap();
+                *count += 1;
+                if *count == iterations {
+                    println!("No match found");
+                    let (lock, cvar) = &*stop_signal;
+                    let mut stop = lock.lock().unwrap();
+                    *stop = true;
+                    cvar.notify_all();
+                }
+            }
+            // Send thread id to main thread
+            sender.send(hash_index);
+            
+        });
+        handles.push(handle);
+    }
+
+    // Wait for any thread to signal to stop
+    let (lock, cvar) = &*stop_signal;
+    let mut found_sig = lock.lock().unwrap();
+    while !*found_sig{
+        found_sig = cvar.wait(found_sig).unwrap();
     }
     let elapsed = now.elapsed();
-    if !found {
-        println!("No match found");
+
+    println!("Stopping all threads...");
+    // Close the sender to signal other threads to stop
+    drop(sender);
+
+    // Wait for all threads to finish
+    let mut indexed_hashes = 0;
+    for _ in 0..threads.parse::<usize>().unwrap() {
+        let idx = receiver.recv();
+        if idx.is_ok() && index {
+            indexed_hashes += idx.unwrap().lock().unwrap().len();
+        }
     }
+
+    println!("All threads have finished.");
+    println!("Indexed {} Hashes", indexed_hashes);
     println!("Time elapsed: {:?}", elapsed);
 
 
